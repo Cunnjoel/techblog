34d20da45781a8b6465ac2e426ce3670c9f2c8d1
Cunnjoel <cunningham.joel84@gmail.com>
2021-05-15 15:49:53 -0500

    update repo

---
+++ controllers/admin-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    const postData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('/', { 
+      posts, 
+      loggedIn: req.session.loggedIn 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      ...post,
+      loggedIn: req.session.loggedIn
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('post', {
+      ...user,
+      loggedin: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,63 +1,22 @@
 const router = require('express').Router();
 const { User, Post } = require('../models');
 
-// GET all galleries for homepage
+
 router.get('/', async (req, res) => {
   try {
-    const dbBlogData = await User.findAll({
+    const postData = await Post.findAll({
       include: [
-        {
-          model: Post,
-          attributes: ['body', 'title'],
-        },
+        User
       ],
     });
 
-    const users = dbBlogData.map((user) =>
+    const users = postData.map((user) =>
       user.get({ plain: true })
     );
     res.render('homepage', {
       users,
       loggedIn: req.session.loggedIn,
     });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// GET one gallery
-router.get('/user/:id', async (req, res) => {
-  try {
-    const dbUserData = await User.findByPk(req.params.id, {
-      include: [
-        {
-          model: Post,
-          attributes: [
-            'id',
-            'title',
-            'body',
-            'userId',
-          ],
-        },
-      ],
-    });
-
-    const gallery = dbBlogData.get({ plain: true });
-    res.render('gallery', { user, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// GET one painting
-router.get('/post/:id', async (req, res) => {
-  try {
-    const dbPostData = await Post.findByPk(req.params.id);
-
-    const post = dbPaintingData.get({ plain: true });
-    res.render('painting', { post, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
